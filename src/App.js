3b8d66ad911197094d40b0098e7d9a805b0278ec
National-student <sanjarshopulatov62@gmail.com>
2022-06-09 07:59:42 +0500

    initial list

--- src/App.js
+++ src/App.js
@@ -1,24 +1,104 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <>
+      <div className='container'>
+        <div className='header'>
+        <a href="#" className='header__logo'>
+          <img src='https://picsum.photo/266/44' alt="logo" width={266} height={44} />
         </a>
-      </header>
+
+        <ul className='header__list'>
+          <li className='list__item'>
+            <a className='item__link' href='#'>Home</a>
+          </li>
+          <li className='list__item'>
+            <a className='item__link' href='#'>Testimonials</a>
+          </li>
+          <li className='list__item'>
+            <a className='item__link' href='#'>Find a doctor</a>
+          </li>
+          <li className='list__item'>
+            <a className='item__link' href='#'>Apps</a>
+          </li>
+          <li className='list__item'>
+            <a className='item__link' href='#'>About us</a>
+          </li>
+        </ul>
+
+        <form className="row g-3 header__form" >
+          <div className="col-auto">
+            <input type="Text" className="form-control" id="inputPassword2" placeholder="Search" />
+          </div>
+          <div className="col-auto">
+            <button type="submit" className="btn btn-primary mb-3">Search</button>
+          </div>
+        </form>
+      </div>
+
+      <div className='main container'>
+          <div className='left-section'>
+            <ul className='left-section__list'>
+              <li className='left-section__item'>
+                    <p className='item-text'>lorem ipsum dolor</p>
+                    <button type="button" class="btn btn-info">Ok</button>
+              </li>
+
+              <li className='left-section__item'>
+                    <p className='item-text'>lorem ipsum dolor</p>
+                    <button type="button" class="btn btn-info">Ok</button>
+              </li>
+
+              <li className='left-section__item'>
+                  <p className='item-text'>lorem ipsum dolor</p>
+                  <button type="button" class="btn btn-info">Ok</button>                
+              </li>
+
+              <li className='left-section__item'>
+                    <p className='item-text'>lorem ipsum dolor</p>
+                    <button type="button" class="btn btn-info">Ok</button>
+              </li>
+
+              <li className='left-section__item'>
+                    <p className='item-text'>lorem ipsum dolor</p>
+                    <button type="button" class="btn btn-info">Ok</button>
+              </li>
+
+              <li className='left-section__item'>
+                  <p className='item-text'>lorem ipsum dolor</p>
+                  <button type="button" class="btn btn-info">Ok</button>                
+              </li>
+
+              <li className='left-section__item'>
+                    <p className='item-text'>lorem ipsum dolor</p>
+                    <button type="button" class="btn btn-info">Ok</button>
+              </li>
+
+              <li className='left-section__item'>
+                    <p className='item-text'>lorem ipsum dolor</p>
+                    <button type="button" class="btn btn-info">Ok</button>
+              </li>
+
+              <li className='left-section__item'>
+                  <p className='item-text'>lorem ipsum dolor</p>
+                  <button type="button" class="btn btn-info">Ok</button>                
+              </li>
+            </ul>
+          </div>
+
+          <div className='right-section'>
+            <div className='card-section'>
+                <ul className='right-list'>
+                  <li className='right-item'>lorem</li>
+                  <li className='right-item'>lorem</li>
+                  <li className='right-item'>lorem</li>
+                </ul>
+            </div>
+          </div>
+      </div>
     </div>
+    </>
   );
 }
 
